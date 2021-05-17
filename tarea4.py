209e85b0c1b298dc6be5c54feac0c04305d6f818
celimo <celimojosepa@gmail.com>
2021-05-17 16:37:42 -0600

    Utilización de pygad Se modifica el código para emplear la libraria de pygad en lugar de hacerlo a mano

--- tarea4.py
+++ tarea4.py
@@ -1,72 +1,111 @@
-import control as co
-import numpy as np
 import matplotlib.pyplot as plt
+import matlab.engine
+import pygad
 
-def fun_Tran(A):
-    # A es el cromosoma con su estructura definida, arreglo de 15 espacios
-    G1 = co.tf([A[0][0]], [A[1][0], A[1][0]*A[2][0], 0])
-    G2 = co.tf([A[0][1], 0], [A[1][1], A[1][1]*A[2][1]+A[3][0]])
-    G3 = co.tf([A[0][2], A[0][2]*A[2][2], 0], [A[1][2]])
+# Se conecta con el entorno de matlab
+eng = matlab.engine.start_matlab()
 
-    H1 = co.tf([A[4][0]*A[5][0], A[5][0]], [1])
-    H2 = co.tf([A[4][1]*A[5][1], A[5][1], 0], [1])
-    H3 = co.tf([A[5][2], 0], [1])
+# Se define la función objetiva
+def funcCalidad(solution, solution_idx):
+    A1 = float(solution[0])
+    A2 = float(solution[1])
+    A3 = float(solution[2])
+    B1 = float(solution[3])
+    B2 = float(solution[4])
+    B3 = float(solution[5])
+    C1 = float(solution[6])
+    C2 = float(solution[7])
+    C3 = float(solution[8])
+    F1 = float(solution[9])
+    F2 = float(solution[10])
+    D2 = float(solution[11])
+    K1 = float(solution[12])
+    K2 = float(solution[13])
+    K3 = float(solution[14])
 
-    num = G1*G2*G3 + G1*H1*G3
-    den = 1 + G1*G2*G3 + G2*H2 + G2*H3 + H1*H3 + G1*H1*G3
+    salida = eng.funcObjetiva(A1,A2,A3,B1,B2,B3,C1,C2,C3,D2,F1,F2,K1,K2,K3)
 
-    funct = num/den
+    return -salida
 
-    return funct
 
+# Función para detener el entrenamiento
+def stopFunction(instance):
+    if instance.best_solution()[1] >= -1:
+        return "stop"
+
+
+# Número máximo de iteraciones o de generaciones
+num_generations = 100
+
+# Número de soluciones a ser seleccionados como padres
+num_parents_mating = 25
+
+# Función objetiva definida anteriormente que debe tener dos parámetros de entrada
+fitness_func = funcCalidad
+
+# Número de poblaciones
+sol_per_pop = 50
+
+# Número de genes en cada cromosoma
+num_genes = 15
+
+# Rango más bajo de los alelos
+init_range_low = -10
+
+# Rango más alto de los alelos
+init_range_high = 10
+
+# Tipo de selección de padres
 '''
-Definición del cromosoma
-
-Cromosoma = [[A1, A2, A3], [B1, B2, B3], [C1, C2, C3], [D2], [F1, F2], [K1, K2, K3]]
-
-Definición del gen: Cada elemento del cromosoma es un vector con dimensiones variables
-
-
-Alelos: Como primera aproximación se consideran valores entre -10 y 10
+"sss"(for steady-state selection)
+'"rws"(for roulette wheel selection)
+"sus"(for stochastic universal selection)
+"rank"(for rank selection)
+"random"(for random selection)
+"tournament"(for tournament selection)
 '''
+parent_selection_type = "tournament"
 
-Cm = []
+# Seleccionar la cantidad de padres que se mantienen en la siguiente generación
+# -1: Mantiene todos los padres
+keep_parents = 5
 
-Cm.append([1,2,3])
-Cm.append([8,5,6])
-Cm.append([7,8,9])
-Cm.append([20])
-Cm.append([11,12])
-Cm.append([13,14,15])
+# Operaciones de cruce genético
+crossover_type = "single_point"
 
-sys = fun_Tran(Cm)
+# Tipo de mutación
+mutation_type="random"
 
-print(sys)
+mutation_probability=0.5
 
-'''
-t = np.linspace(0, 1000, 100000)
-t1, y1 = co.step_response(sys, t)
+# No permite genes duplicados
+allow_duplicate_genes = False
 
-plt.plot(t1, y1)
-plt.grid()
-plt.show()
+on_generation = stopFunction
 
-mag, pha, frec = co.bode_plot(sys)
+ga_instance = pygad.GA(num_generations=num_generations,
+                       num_parents_mating=num_parents_mating,
+                       fitness_func=fitness_func,
+                       sol_per_pop=sol_per_pop,
+                       num_genes=num_genes,
+                       init_range_low=init_range_low,
+                       init_range_high=init_range_high,
+                       parent_selection_type=parent_selection_type,
+                       keep_parents=keep_parents,
+                       mutation_probability=mutation_probability,
+                       crossover_type=crossover_type,
+                       allow_duplicate_genes=allow_duplicate_genes,
+                       on_generation=on_generation)
 
-plt.show()
+ga_instance.run()
 
-plt.plot(frec, mag)
-plt.yscale('log')
-plt.xscale('log')
-plt.grid(True, which="both")
-plt.show()
+ga_instance.plot_result()
 
-pha = np.array(pha)
+solution, solution_fitness, solution_idx = ga_instance.best_solution()
+print("Mejor individuo del evolutivo: {solution} ".format(solution=solution))
+print("Calidad de la mejor solución = {solution_fitness}".format(solution_fitness=solution_fitness))
+print("Índice de la mejor solución: {solution_idx}".format(solution_idx=solution_idx))
 
-pha = pha*180/(np.pi)
-
-plt.plot(frec, pha)
-plt.xscale('log')
-plt.grid(True, which="both")
-plt.show()
-'''
+# Se guarda el resultado del entrenamiento
+filename = 'genetic'
+ga_instance.save(filename=filename)
